1f364d9c7787cf1289fa64dd382fdcd2138bbb82
Bewis09 <bewis09minecraft@gmail.com>
2025-02-13 16:38:58 +0100

    initial commit: add basic structure for the Communicated mod with mixins, data generation, and Gradle setup

---
+++ src/client/kotlin/bewis09/communicated/CommunicatedClient.kt
@@ -0,0 +1,9 @@
+package bewis09.communicated
+
+import net.fabricmc.api.ClientModInitializer
+
+object CommunicatedClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/kotlin/bewis09/communicated/CommunicatedDataGenerator.kt
@@ -0,0 +1,9 @@
+package bewis09.communicated
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object CommunicatedDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
